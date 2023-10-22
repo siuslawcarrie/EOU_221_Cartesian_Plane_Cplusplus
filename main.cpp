981ffd84d33ff21012e996e2b6598f84b9cb1be8
siuslawcarrie <102423932+siuslawcarrie@users.noreply.github.com>
2023-10-22 14:41:44 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,21 @@
+/*Write a program that prompts the user to input the (x, y) coordinate of a point
+ * in a Cartesian plane. The program should then output a message indicating
+ * whether the point is the origin,
+is located on the x or y axis, or appears in a particular quadrant.
+ */
+
+//VARIABLES
+//int x // x coordinate
+// int y // y coordinate
+//END VARIABLES
+
+//IF STATEMENT
+/*
+ * if x == 0 and y == 0, display origin msg
+ * if x == 0, display (x,y) and on the y coordinate
+ * if y == 0, display (x,y) and on the x coordinate
+ * if x > 0 AND y > 0, display (x,y) and Q1
+ * if x < 0 and y > 0, display (x,y) and Q2
+ * if x < 0 and y < 0, display (x,y) and Q3
+ * if x > 0 and y < 0, display (x,y) and Q4
+ */
